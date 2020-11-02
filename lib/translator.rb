46a8f4c4417029e37b9fbb7d315269abbb232653
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-11-02 22:14:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,22 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(path)
+  final_hash = {}
+  YAML.load_file(path).each do |key, value|
+    final_hash[key] = {}
+    final_hash[key][:english] = value[0]
+    final_hash[key][:japanese] = value[1]
+end
+  final_hash
+end
+
+def get_english_meaning(path, emoticon)
+  load_library(path).each do |key, value|
+    binding.pry
+  end
 end
 
 def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+  
 end
